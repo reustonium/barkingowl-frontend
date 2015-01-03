fcfeb748fa0d3a4369251b5e164a2c7e68343da4
andy <andrew.ruestow@gmail.com>
2015-01-02 21:55:41 -0500

    idk wtf is wrong

--- server.py
+++ server.py
@@ -145,8 +145,6 @@ def app_url():
 
 if __name__ == "__main__":
     print "Web Application Starting ..."
-    
     host = '0.0.0.0'
-    port = int(os.environ.get('PORT', 8067)
-
-    fa = app.run()#host=host, port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host=host, port=port)
