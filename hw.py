9676dec2bdbeafb3a8756b752a32ec3c20ce68f1
andy <andrew.ruestow@gmail.com>
2015-01-02 21:38:07 -0500

    port biznaz

--- hw.py
+++ hw.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,9 +7,6 @@ def hello():
 	return "Hello World"
 
 if __name__ == "__main__":
-    print "Web Application Starting ..."
-    
-    host = '0.0.0.0'
-    port = int(os.environ.get('PORT', 8067)
-
-    app.run(host=host, port=port)
+	print "Web Application Starting ..."
+	host = '0.0.0.0'
+	app.run(host=host)
