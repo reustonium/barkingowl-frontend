2820d4f49ebb55ebee6208cf5b51a628a69950e2
andy <andrew.ruestow@gmail.com>
2015-01-02 21:51:59 -0500

    no moar host

--- server.py
+++ server.py
@@ -149,4 +149,4 @@ if __name__ == "__main__":
     host = '0.0.0.0'
     port = int(os.environ.get('PORT', 8067)
 
-    fa = app.run(host=host)#, port=port)
+    fa = app.run()#host=host, port=port)
