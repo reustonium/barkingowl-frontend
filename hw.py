4226d20fa4a2468f2114a97b5b82a00d7b9689d7
andy <andrew.ruestow@gmail.com>
2015-01-02 21:42:23 -0500

    ports

--- hw.py
+++ hw.py
@@ -9,4 +9,5 @@ def hello():
 if __name__ == "__main__":
 	print "Web Application Starting ..."
 	host = '0.0.0.0'
-	app.run(host=host)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host=host, port=port)
